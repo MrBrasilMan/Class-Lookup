c3710d8e3dd56ff828101350b837313b5e9f98bc
MrBrazil <lucasfrias878@gmail.com>
2021-01-15 21:46:11 +0000

    This is not really for most people unless you have an admin who controls whether you create G Meets or not. But it is good to push this anyway.

--- main.py
+++ main.py
@@ -3,3 +3,15 @@ print ("Class Lookup")
 import requests, os
 from time import sleep
 print ("____________________")
+url = input ("Google Meet URL >")
+while True:
+  #Robj is what will be used to view information about the website using the request module. Cool, huh?
+  robj = requests.get(url)
+  print ("Pinging " + url)
+  #A varaity of checks are done in order to verify that the URL is a valid, running Google Meet meeting before finally beeping or not.
+  beepformeet = True
+  if robj.status_code() != 200:
+    beepformeet = False
+  #This next bit is dependent on how Google Meet is structured. If a Google Meet is not available, it will go to a whoops error message only when an administrator has banned their class to create their own Google Meets.
+  if "href=" in robj.text():
+    pass
